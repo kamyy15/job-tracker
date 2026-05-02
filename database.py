d9073d744cb06c5d63b5745de1f16af9d6fda3d8
Briathna Bubick <149510120+kamyy15@users.noreply.github.com>
2026-05-01 20:19:05 -0400

    Add SQLite database setup and storage functions

--- database.py
+++ database.py
@@ -1,8 +1,11 @@
 import sqlite3
 
 def init_db():
+    # connect to the database
     connection = sqlite3.connect("jobs.db") # like open for files; creates a jobs.db file if it doesn't exist, or connects to it if it does.
+    # create a cursor
     cursor = connection.cursor()
+    # execute the SQL
     cursor.execute(
         """CREATE TABLE IF NOT EXISTS jobs (
         id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -21,7 +24,9 @@ def init_db():
         resume_version TEXT,
         notes TEXT
     )""") #runs sql statement
+    # commit
     connection.commit() # saves changes
+    #close
     connection.close() # closes connection when done
 
 
--- storage.py
+++ storage.py
@@ -7,7 +7,22 @@ def get_all_jobs():
     cursor.execute("""
     SELECT * FROM jobs
     """)
-    jobs = cursor.fetchall() # call it and store it in a variable and return it.
+    jobs = cursor.fetchall() # reads all and stores them in a Python variable so we can work with them outside of database. Loading it into Python memory.
     connection.close()
     return jobs # has to be last as once Python hits return it exits function immediately
     
+def add_job_db(date_posted, company_name, position, location, work_environment, employment_type, pay, date_applied, deadline, status, contact, URL, resume_version, notes): #don't need jobs or id parameters - id parameter is being handles by AUTOINCREMENT.
+    connection = sqlite3.connect("jobs.db")
+    cursor = connection.cursor()
+    cursor.execute("""
+    INSERT INTO jobs ( date_posted, company_name, position, location, work_environment, employment_type, pay, date_applied, deadline, status, contact, URL, resume_version, notes) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
+    """, ( date_posted, company_name, position, location, work_environment, employment_type, pay, date_applied, deadline, status, contact, URL, resume_version, notes)) # sqlite takes what's passed into the tuple and puts them in place of the value placeholders.
+    connection.commit()
+    connection.close()
+
+def update_job_db():
+    connection = sqlite3.connect("jobs.db")
+    cursor = connection.cursor()
+    cursor.execute("""
+    UPDATE jobs SET status
+""")
